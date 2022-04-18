8d68a35db4fabecc7b43f05b7f31404d2596782b
gilmoright <artem.dahaka@gmail.com>
2022-04-18 19:51:10 +0300

    Добавил эксперимент с обычным классификатором только по y

--- MultilabelML.py
+++ MultilabelML.py
@@ -14,7 +14,8 @@ from sklearn.preprocessing import OneHotEncoder
 import simpletransformers
 from sklearn.metrics import classification_report
 from simpletransformers.classification import (
-    MultiLabelClassificationModel, MultiLabelClassificationArgs
+    MultiLabelClassificationModel, MultiLabelClassificationArgs,
+    ClassificationModel, ClassificationArgs
 )
 from RobotCommandClassifier import utils
 
@@ -33,68 +34,79 @@ if __name__=="__main__":
         type=str,
         help="name of experiment to get from config file")
     args = parser.parse_args()
+    # setup the experiment
     with open(args.config_file, "r") as f:
         experiments = yaml.safe_load(f)
     EXPERIMENT_NAME = args.experiment_name
     CONFIG = experiments[EXPERIMENT_NAME]
 
-    if os.path.exists(CONFIG["Report"]["report_dir"]) and len(os.listdir(CONFIG["Report"]["report_dir"])>0):
+    if os.path.exists(CONFIG["Report"]["report_dir"]) and len(os.listdir(CONFIG["Report"]["report_dir"]))>0:
         raise ValueError("report_dir is not empty: {}".format(CONFIG["Report"]["report_dir"]))
     if not os.path.exists(CONFIG["Report"]["report_dir"]):
         os.makedirs(CONFIG["Report"]["report_dir"])
     with open(os.path.join(CONFIG["Report"]["report_dir"], "config.json"), "w") as f:
         json.dump(CONFIG, f)
 
-
+    # Prepare data
     train_x_df, train_y_df, valid_x_df, valid_y_df, test_x_df, test_y_df = utils.load_data(**CONFIG["Data"])
 
-    enc = OneHotEncoder()
-    enc.fit(train_y_df.values)
+    if CONFIG["Type"] == "simple_ml_multilabel_classifier":
+        enc = OneHotEncoder()
+        enc.fit(train_y_df.values)
 
-    labels = []
-    encoded_labels = enc.transform(train_y_df.values).toarray().astype(int)
-    for i in range(train_y_df.shape[0]):
-        labels.append(encoded_labels[i].tolist())
-    train_df = pd.DataFrame(list(zip(train_x_df, labels)))
+        labels = []
+        encoded_labels = enc.transform(train_y_df.values).toarray().astype(int)
+        for i in range(train_y_df.shape[0]):
+            labels.append(encoded_labels[i].tolist())
+        train_df = pd.DataFrame(list(zip(train_x_df, labels)))    
+    else:
+        train_df = pd.concat([train_x_df, train_y_df], axis=1)
     train_df.columns = ["text", "labels"]
 
-
-    model_args = MultiLabelClassificationArgs(**CONFIG["Model"]["Args"])
-    # Create a MultiLabelClassificationModel
-    model = MultiLabelClassificationModel(
-        CONFIG["Model"]["model_type"],
-        CONFIG["Model"]["model_name"],
-        num_labels=len(labels[0]),
-        use_cuda=True,    
-        args=model_args,
-    )
+    # Create model
+    if CONFIG["Type"] == "simple_ml_multilabel_classifier":
+        model_args = MultiLabelClassificationArgs(**CONFIG["Model"]["Args"])
+        # Create a MultiLabelClassificationModel
+        model = MultiLabelClassificationModel(
+            CONFIG["Model"]["model_type"],
+            CONFIG["Model"]["model_name"],
+            num_labels=len(labels[0]),
+            use_cuda=True,    
+            args=model_args,
+        )
+    elif CONFIG["Type"] == "simple_ml_classifier":
+        model_args = ClassificationArgs(**CONFIG["Model"]["Args"])
+        model = ClassificationModel(
+            CONFIG["Model"]["model_type"],
+            CONFIG["Model"]["model_name"],
+            num_labels=len(train_y_df.iloc[:,0].unique()),
+            use_cuda=True,    
+            args=model_args,
+        )
+    else:
+        raise ValueError("unknown Type of experiment:{}".format(CONFIG["Type"]))
 
     # Train the model
     model.train_model(train_df, overwrite_output_dir=True)
 
+    # Predict and evaluate
     predictions, raw_outputs = model.predict(valid_x_df.values.tolist())
-
-    predictions_2 = np.zeros((len(predictions), len(enc.categories_)))
-    for i in range(len(predictions)):
-        shift = 0
-        for j in range(len(enc.categories_)):
-            predictions_2[i,j] = np.argmax(raw_outputs[i, shift:shift+len(enc.categories_[j])])
-            shift += len(enc.categories_[j])
-
-    result = utils.calculate_metrics_2(valid_y_df, predictions_2, display=True)
+    if CONFIG["type"] == "simple_ml_multilabel_classifier":
+        predictions_2 = np.zeros((len(predictions), len(enc.categories_)))
+        for i in range(len(predictions)):
+            shift = 0
+            for j in range(len(enc.categories_)):
+                predictions_2[i,j] = np.argmax(raw_outputs[i, shift:shift+len(enc.categories_[j])])
+                shift += len(enc.categories_[j])
+        predictions = predictions_2
+    result = utils.calculate_metrics_2(valid_y_df, predictions, display=True)
     with open(os.path.join(CONFIG["Report"]["report_dir"], "classes_report.json"), "w") as f:
         json.dump(result, f)
 
-    result_avg = utils.calculate_metrics(valid_y_df, predictions_2, config={
+    result_avg = utils.calculate_metrics(valid_y_df, predictions, config={
         "report_metrics": CONFIG["Report"]["report_metrics"]
     })
     with open(os.path.join(CONFIG["Report"]["report_dir"], "avg_report.json"), "w") as f:
         json.dump(result_avg, f)
-    for k, v in result_avg.items():
-        print(np.round(v*100), "\t", k)
-
-    result_avg = utils.calculate_metrics(test_y_df, predictions_2, config={
-        "report_metrics": ["correct_samples_perc", "avg_macro_f1", "avg_acc", "class_acc", "class_macro_f1"]
-    })
     for k, v in result_avg.items():
         print(np.round(v*100), "\t", k)
---
+++ train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -J LMclassifier
+#SBATCH -D /s/ls4/users/grartem/RL_robots/CommandClassifier
+#SBATCH -o /s/ls4/users/grartem/RL_robots/CommandClassifier/Logs/train_%x_%j.out
+#SBATCH -e /s/ls4/users/grartem/RL_robots/CommandClassifier/Logs/train_%x_%j.err
+#SBATCH -p hpc5-el7-gpu-3d
+#SBATCH -n 1
+#SBATCH --gres=gpu:k80:1
+#SBATCH --time=12:00:00
+
+export HOME=/s/ls4/users/grartem
+#export PATH=$HOME/anaconda3/envs/rl_robots/bin:$PATH
+export PATH=$HOME/anaconda3/envs/simptr/bin:$HOME/RL_robots/CommandClassifier:$PATH
+
+export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:$HOME/installation_dists/cudnn-9.0-linux-x64-v7.1.ga/lib64:
+module load gcc/7.3.0
+# RandomForestClassifier ExtraTreesClassifier ExtraTreeClassifier DecisionTreeClassifier KNeighborsClassifier RadiusNeighborsClassifier
+python MultilabelML.py --config_file Configs/SimpleLM.yml --experiment_name rubert_tiny2_multilabel_fold0
+python MultilabelML.py --config_file Configs/SimpleLM.yml --experiment_name rubert_tiny2_multilabel_fold1
+python MultilabelML.py --config_file Configs/SimpleLM.yml --experiment_name rubert_tiny2_multilabel_fold2
+python MultilabelML.py --config_file Configs/SimpleLM.yml --experiment_name rubert_tiny2_multilabel_fold3
+python MultilabelML.py --config_file Configs/SimpleLM.yml --experiment_name rubert_tiny2_multilabel_fold4
